70795e62bd015449daf247ab1cbf39808028582f
Jon-Carlos Rivera <imbcmdth@hotmail.com>
2013-05-05 21:07:34 -0400

    Kid tested; mother approved.

---
+++ index.js
@@ -0,0 +1,105 @@
+(function (root, factory) {
+		"use strict";
+
+		if (typeof exports === 'object') {
+			module.exports = factory();
+		} else if (typeof define === 'function' && define.amd) {
+			define(factory);
+		} else {
+			root.UltraDeepClone = factory();
+		}
+	}(this, function () {
+
+		var functionPropertyFilter = [
+			"caller",
+			"arguments",
+			"prototype"
+		];
+
+		var primitiveCloner  = makeCloner(clonePrimitive);
+
+		var cloneFunctions = {
+			"[object Null]" : primitiveCloner ,
+			"[object Undefined]" : primitiveCloner ,
+			"[object Number]" : primitiveCloner ,
+			"[object String]" : primitiveCloner ,
+			"[object Boolean]" : primitiveCloner ,
+			"[object RegExp]" : makeCloner(cloneRegExp),
+			"[object Date]" : makeCloner(cloneDate),
+			"[object Function]" : makeRecursiveCloner(makeCloner(cloneFunction), functionPropertyFilter),
+			"[object Object]" : makeRecursiveCloner(makeCloner(cloneObject)),
+			"[object Array]" : makeRecursiveCloner(makeCloner(cloneArray))
+		};
+
+		function makeCloner(cloneThing) {
+			return function(thing, thingStack, copyStack) {
+				thingStack.push(thing);
+				var copy = cloneThing(thing);
+				copyStack.push(copy);
+				return copy;
+			};
+		}
+
+		function clonePrimitive(primitive) {
+			return primitive;
+		}
+
+		function cloneRegExp(regexp) {
+			return new RegExp(regexp);
+		}
+
+		function cloneDate(date) {
+			return new Date(date.getTime());
+		}
+
+		function cloneFunction(fn) {
+			var copy = Function("return " + fn.toString() + ";")();
+			copy.prototype = Object.getPrototypeOf(fn);
+			return copy;
+		}
+
+		// This will not properly clone `constructed` objects because
+		// it is impossible to know with what arguments the constructor
+		// was originally invoked.
+		function cloneObject(object) {
+			return Object.create(Object.getPrototypeOf(object));
+		}
+
+		function cloneArray(array) {
+			return [];
+		}
+
+		function makeRecursiveCloner(cloneThing, propertyFilter) {
+			return function(thing, thingStack, copyStack) {
+				var clone = this;
+
+				return Object.getOwnPropertyNames(thing).filter(function(prop){
+					return !propertyFilter || propertyFilter.indexOf(prop) === -1;
+				}).
+				reduce(function(copy, prop) {
+					var thingOffset = thingStack.indexOf(thing[prop]);
+
+					if (thingOffset === -1) {
+						copy[prop] = clone(thing[prop]);
+					} else {
+						copy[prop] = copyStack[thingOffset];
+					}
+
+					return copy;
+				}, cloneThing(thing, thingStack, copyStack));
+			};
+		}
+
+		return function _ultraDeepCopy(source) {
+
+			var thingStack = [],
+				copyStack = [];
+
+			function clone(thing){
+				var typeOfThing = Object.prototype.toString.call(thing);
+				return cloneFunctions[typeOfThing].call(clone, thing, thingStack, copyStack);
+			}
+
+			return clone(source);
+		};
+}));
---
+++ test/clone-test.js
@@ -0,0 +1,164 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    deepCopy = require('../');
+
+var testConstructor = function() { this.foo = "test_foo"; };
+testConstructor.prototype = { bar: "test_bar" };
+testConstructor.baz = "test_baz";
+
+var testTimestamp = Date.now();
+var testDate = new Date(testTimestamp);
+var testNumber = 12345;
+var testString = "12345";
+var testArray = ["foo", "bar" ];
+var testRegExp = /./g;
+var testBoolean = true;
+var testFunction = function _test() { return "test"; };
+var testObject = { foo:"test_foo" };
+
+var testComplexObject = {
+	number : testNumber,
+	string : testString,
+	regexp : new RegExp(testRegExp),
+	"null" : null,
+	"undefined" : undefined,
+	boolean : testBoolean,
+	date : testDate,
+	"function" : testFunction,
+	constructor : testConstructor,
+	constructed : new testConstructor(),
+	object : testObject,
+	array : testArray,
+	cycles : {},
+	cycles2 :{}
+};
+
+//add some cycles
+testComplexObject.cycles.parent = testComplexObject;
+testComplexObject.cycles2.other = testComplexObject.cycles;
+testComplexObject.cycles.complex = testComplexObject.object;
+
+//we pre-exec the regex to add some internal state
+testComplexObject.regexp.exec(testString);
+
+vows.describe('Deep Clone').addBatch({
+	'Number': {
+		topic: function(){ return deepCopy(testNumber); },
+		'Correct Value' : function (clone) {
+			assert.equal (clone, testNumber);
+		},
+		'Changes Independently' : function (clone) {
+			clone = 0;
+			assert.equal (testNumber, 12345);
+		}
+	},
+	'String': {
+		topic: function(){ return deepCopy(testString); },
+		'Correct Value' : function (clone) {
+			assert.equal (clone, testString);
+		}
+	},
+	'Date': {
+		topic: function(){ return deepCopy(testDate); },
+		'Correct Value' : function (clone) {
+			assert.equal (clone.getTime(), testDate.getTime());
+		}
+	},
+	'Boolean': {
+		topic: function(){ return deepCopy(testBoolean); },
+		'Correct Value' : function (clone) {
+			assert.equal (clone, testBoolean);
+		}
+	},
+	'Array': {
+		topic: function(){ return deepCopy(testArray); },
+		'Correct Value' : function (clone) {
+			assert.deepEqual (clone, testArray);
+		}
+	},
+	'Function': {
+		topic: function(){ return deepCopy(testFunction); },
+		'Correct Value' : function (clone) {
+			assert.equal (clone(), testFunction());
+		}
+	},
+	'Simple Object': {
+		topic: function(){ return deepCopy(testObject); },
+		'Correct Value' : function (clone) {
+			assert.deepEqual (clone, testObject);
+		}
+	},
+	'Complex Object': {
+		topic: function(){ return deepCopy(testComplexObject); },
+
+		'number' : function (clone) {
+			assert.equal (clone.number, testNumber);
+			clone.number = 14;
+			assert.equal (testComplexObject.number, testNumber);
+		},
+		'string': function (clone) {
+			assert.equal (clone.string, testString);
+			clone.string = 'changed';
+			assert.equal (testComplexObject.string, testString);
+		},
+		'regexp': function (clone) {
+			assert.deepEqual (clone.regexp.exec(testString)[0], "1");
+			assert.equal (clone.regexp.exec(testString)[0], "2");
+			assert.equal (testComplexObject.regexp.exec(testString)[0], "2");
+		},
+		'null': function (clone) {
+			assert.equal (clone.null, null);
+		},
+		'undefined' : function(clone) {
+			assert.equal (typeof clone.undefined, 'undefined');
+		},
+		'boolean' : function(clone) {
+			assert.isTrue (clone.boolean);
+			clone.boolean = false;
+			assert.isTrue (testComplexObject.boolean);
+		},
+		'date' : function(clone) {
+			assert.equal (clone.date.getTime(), testTimestamp);
+			clone.date = new Date();
+			assert.equal (testComplexObject.date.getTime(), testTimestamp);
+		},
+		'function' : function(clone) {
+			assert.equal (clone.function(), 'test');
+			assert.equal (clone.function.name, '_test');
+			clone.function.name = 'changed';
+			assert.equal (testComplexObject.function(), 'test');
+			assert.equal (testComplexObject.function.name, '_test');
+		},
+		'constructor' : function(clone) {
+			assert.isFalse (clone.constructor === testComplexObject.constructor);
+			assert.equal (clone.constructor.baz, 'test_baz');
+			clone.constructor.baz = 'changed';
+			assert.equal (testComplexObject.constructor.baz, 'test_baz');
+		},
+		'constructed' : function(clone) {
+			assert.isFalse (clone.constructed === testComplexObject.constructed);
+
+			assert.equal (clone.constructed.foo, 'test_foo');
+			clone.constructed.foo = 'changed';
+			assert.equal (testComplexObject.constructed.foo, 'test_foo');
+
+			assert.equal (clone.constructed.bar, 'test_bar');
+			Object.getPrototypeOf(clone.constructed).bar = 'changed';
+			assert.equal (testComplexObject.constructed.bar, 'changed');
+		},
+		'object' : function(clone) {
+			assert.equal (clone.object.foo, 'test_foo');
+			clone.object.foo = 'changed';
+			assert.equal (testComplexObject.object.foo, 'test_foo');
+		},
+		'array' : function(clone) {
+			assert.deepEqual (clone.array, ['foo', 'bar']);
+			clone.array[0] = 'changed';
+			assert.deepEqual (testComplexObject.array, ['foo', 'bar']);
+		},
+		'cycles' : function(clone) {
+			assert.equal (clone.cycles2.other, clone.cycles);
+			assert.equal (clone, clone.cycles.parent);
+		}
+	}
+}).export(module);
